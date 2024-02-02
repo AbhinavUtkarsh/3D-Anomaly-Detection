c7c18ba2acd944e4f08778ffed9e3b6b964fb292
Matthias <matze.1998@gmx.fr>
2024-02-02 16:53:04 +0100

    put nasnetalarge as default everywhere

--- CPMF/run_main.sh
+++ CPMF/run_main.sh
@@ -4,11 +4,5 @@ exp_name="Testrun"
 # run with ResNet backbone
 # python main.py --category  diamond --n-views 27 --no-fpfh False --data-path $data_dir --exp-name $exp_name --backbone resnet18
 
-# run with Pooling-based Vision Transformer (PiT) backbone
-# python main.py --category  diamond --n-views 27 --no-fpfh False --data-path $data_dir --exp-name $exp_name --backbone pit_s_224
-
-# run with Swin Transformer backbone
-# python main.py --category  diamond --n-views 27 --no-fpfh False --data-path $data_dir --exp-name $exp_name --backbone swin_s3_small_224
-
 # run with NasNet large backbone
 python main.py --category  diamond --n-views 27 --no-fpfh False --data-path $data_dir --exp-name $exp_name --backbone nasnetalarge
