b21c31653ff7ab418776d12854329a2133868c96
Matthias <matze.1998@gmx.fr>
2024-02-04 19:17:12 +0100

    set fpfh to false

--- CPMF/run_main.sh
+++ CPMF/run_main.sh
@@ -2,8 +2,8 @@ data_dir=./datasets/Real_AD_3D_multiview/
 exp_name="Testrun"
 
 # run with ResNet backbone
-# python main.py --category  diamond --n-views 27 --no-fpfh False --data-path $data_dir --exp-name $exp_name --backbone resnet18
+# python main.py --category  diamond --n-views 27 --no-fpfh True --data-path $data_dir --exp-name $exp_name --backbone resnet18
 
 
 # run with NasNet large backbone
-python main.py --category  diamond --n-views 27 --no-fpfh False --data-path $data_dir --exp-name $exp_name --backbone nasnetalarge
+python main.py --category  diamond --n-views 27 --no-fpfh True --data-path $data_dir --exp-name $exp_name --backbone nasnetalarge
